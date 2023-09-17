af36403de94df1828abb1ffb409057b056916aed
--replace-all <anshulgupta4905@gmail.com>
2023-09-18 00:11:32 +0530

    Adding Comments

--- assignments/counter-mini-project/JS/script.js
+++ assignments/counter-mini-project/JS/script.js
@@ -1,19 +1,26 @@
+// <======== Initializing Variables ========>
 const digitElement = document.querySelector("#digit-container span");
 const increment = document.querySelector("#increment");
 const setZero = document.querySelector("#setZero");
 const decrement = document.querySelector("#decrement");
 let num = 0;
 
+
+// <======== JS Program to Increase the Count of the Counter ========>
 increment.addEventListener("click", () => {
     num++;
     digitElement.innerText = num;
 })
 
+
+// <======== JS Program to Reset the Count of the Counter ========>
 setZero.addEventListener("click", () => {
     num = 0;
     digitElement.innerText = num;
 })
 
+
+// <======== JS Program to Decrease the Count of the Counter ========>
 decrement.addEventListener("click", () => {
     num--;
     digitElement.innerText = num;
